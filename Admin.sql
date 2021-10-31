9f2181f6478f969ebcc0ae98555176f629abb8e9
JZN-37 <89829197+JZN-37@users.noreply.github.com>
2021-10-31 21:56:47 +0530

    Commit

---
+++ Admin.sql
@@ -0,0 +1,39 @@
+create database BookStoreApp
+use BookStoreApp
+
+create table Admin
+(
+AId int primary key identity,
+AName nvarchar(50),
+APwd nvarchar(50)
+)
+
+create table Discount
+(
+DId int primary key identity,
+DCouponCode nvarchar(50),
+DDiscountValue float,
+DStatus bit
+)
+
+create table Orders
+(
+UserId int foreign key references Users(Id),
+OrderId int,
+BookTitle nvarchar(50),
+BookCatName nvarchar(50),
+BookISBN nvarchar(50),
+BookYear date,
+BookPrice float,
+BImagePath nvarchar(50),
+BookQty int,
+OrderDate datetime
+)
+
+create proc usp_BeforeInsrtToOrders(@userId int)
+with encryption
+as
+begin
+update Users set UOrderNo=UOrderNo+1 where Id= @userId
+end
+
